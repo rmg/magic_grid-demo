1f7a32d5abdd3899033238b310583270a55a4a08
Ryan Graham <r.m.graham@gmail.com>
2013-04-20 15:25:21 -0700

    Show some examples on the main page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
+    @posts = Post.unscoped
   end
 
   # GET /posts/1
