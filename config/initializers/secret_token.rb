a82c44cd15b5f6180712bb1185d57557fb46a762
Ryan Graham <r.m.graham@gmail.com>
2013-04-20 12:39:07 -0700

    Initial commit
    rails new magic_grid_demo --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicGridDemo::Application.config.secret_token = '7263779d3e66106aa9ada7417573911847ba16b3ba91ae35d2df27d927b5e9428ce39c48de2c52e42413777498e551190ce7746be16bc5f810da268c1b5eac04'
